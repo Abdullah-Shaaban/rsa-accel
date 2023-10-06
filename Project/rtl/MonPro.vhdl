ee2494407de0b853869225ec3fe71f53c8fec4a5
Abdullah Allam <s-abdullahshaaban@zewailcity.edu.eg>
2023-10-06 20:27:45 +0200

    Higher freq, less area

--- Project/rtl/MonPro.vhdl
+++ Project/rtl/MonPro.vhdl
@@ -30,9 +30,9 @@ architecture rtl of MonPro is
   -- Intermediate signals
   signal count_en     : std_logic;
   signal cnt_max      : std_logic;
-  signal done_del     : std_logic;
+  signal done_reg     : std_logic;
   signal pre_process  : std_logic;
---  signal post_process : std_logic;
+  signal sel2         : std_logic_vector(1 downto 0);
   --signal U_minus_N    : signed(k downto 0);
   signal U_minus_N    : unsigned(k-1 downto 0);
   -- Adder
@@ -45,31 +45,21 @@ begin
   -- Data path
   data_path : process (all)
     variable sel1 : std_logic;
-    variable sel2 : std_logic_vector(2 downto 0);
     variable u0   : std_logic;
   begin
     -- Mux1: Select the first input of the adder
     sel1 := pre_process;
-    if sel1 = '1' then
-      add_in1 <= N_reg;
-    else
-      add_in1 <= U_reg;
+    if sel1 = '1' then  add_in1 <= N_reg;
+    else                add_in1 <= U_reg;
     end if;
     -- Mux2: Select the second input of the adder
     u0  := U_reg(0) xor (A_reg(0) and B_reg(0));
-    sel2 := u0 & A_reg(0) & sel1;
-    case sel2 is
-      when "000" =>
-        add_in2 <= (others => '0');
-      -- when "010" =>
-      --   add_in2 <= '0' & B_reg;
-      when "100" =>
-        add_in2 <= '0' & N_reg;
-      when "110" =>
-        add_in2 <= B_plus_N_reg;
-      when others =>
-        add_in2 <= '0' & B_reg;
-    end case;
+    sel2 <= (u0 & A_reg(0)) and (not(sel1) & not(sel1));
+    case? sel2 is
+      when "0-" =>  add_in2 <= '0' & B_reg;
+      when "10" =>   add_in2 <= '0' & N_reg;
+      when "11" =>   add_in2 <= B_plus_N_reg;
+    end case?;
     -- Adder
     add_out <= ('0' & add_in1) + add_in2;
     -- Subtractor: makes sure the output is less than N
@@ -81,19 +71,24 @@ begin
   process (clk)
   begin
     if rising_edge(clk) then
+      
       if load = '1' then -- Pulse
-        A_reg <= A & '0'; -- Compensate for 1 early shift. Avoids making MUX
         B_reg <= B;
         N_reg <= N;
-      else
-        A_reg <= '0' & A_reg(k downto 1); -- Shift right
       end if;
-      if pre_process = '1' then
-        U_reg <= (others => '0'); -- Initialize U_reg with zero just before starting the counter next cycle
-        B_plus_N_reg <= add_out;
-      else
-        U_reg <= add_out(k downto 1); -- The U/2 is done here by omitting U_out(0)
+      
+      if pre_process = '1' then   B_plus_N_reg <= add_out;
       end if;
+
+      if load = '1' then    A_reg <= A & '0';   -- Compensate for 1 early shift. Avoids making MUX
+      else                  A_reg <= '0' & A_reg(k downto 1);
+      end if;
+      
+      if pre_process = '1' then   U_reg <= (others => '0'); -- Initialize U_reg with zero just before starting the counter next cycle
+      elsif sel2 = "00" then      U_reg <= '0' & U_reg(k-1 downto 1);
+      else                        U_reg <= add_out(k downto 1); -- U/2
+      end if;
+
     end if;
   end process;
 
@@ -105,7 +100,7 @@ begin
     if rst_n = '0' then
       pre_process <= '0';
       count_en    <= '0';
-      done_del    <= '0';
+      done_reg    <= '0';
     elsif rising_edge(clk) then
       pre_process <= load;
       if pre_process = '1' then
@@ -113,7 +108,7 @@ begin
       elsif cnt_max = '1' then
         count_en <= '0';
       end if;
-      done_del <= cnt_max;
+      done_reg <= cnt_max;
     end if;
   end process;
 
@@ -129,12 +124,10 @@ begin
     end if;
   end process;
 
-  -- Final Output
+  -- Final Output ___ Optimize later by omitting the "<" comparison, and doing the subtraction alone.
 --  out_p <= U_reg when (U_minus_N(k)) else  -- Because U < N means result of U-N is negative, i.e., sign=1
---          unsigned(U_minus_N(k-1 downto 0)); -- Optimize later by omitting the comparisong, and doing the subtraction alone.
-   out_p <= U_reg when (U_reg<N_reg) else  -- Because U < N means result of U-N is negative, i.e., sign=1
-        U_minus_N; -- Optimize later by omitting the comparisong, and doing the subtraction alone.
-                                          -- Idea:  an extra register for -n and reuse the adder
-  done <= done_del;
+--          unsigned(U_minus_N(k-1 downto 0));
+  out_p <= U_reg when (U_reg<N_reg) else U_minus_N;
+  done <= done_reg;
 
 end architecture rtl;
